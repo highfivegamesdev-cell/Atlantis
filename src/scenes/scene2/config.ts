70b5cfa74ac256bb48f5205e4476dbea5e4d65c2
lukasz-kowalski <32309180+lukasz-kowalski@users.noreply.github.com>
2025-11-12 23:15:28 +0100

    change riddle text (#15)
    restore config

--- src/scenes/scene2/config.ts
+++ src/scenes/scene2/config.ts
@@ -13,6 +13,5 @@ export const puzzleConfig = {
     thumbnail: "/images/scenes/scene2/puzzles/riddle-thumbnail.png",
     background: "/images/scenes/scene2/puzzles/riddle-background.png",
     puzzle: "/images/scenes/scene2/puzzles/riddle-puzzle.png",
-    text: "/images/scenes/scene2/puzzles/riddle-text.png",
   },
 };
